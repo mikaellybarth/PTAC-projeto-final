e7e6d2198fe5feedf970e12477b781a253933bef
mikaellybarth <wwwmikasouza06@gmail.com>
2023-11-28 07:31:27 -0400

    commit

---
+++ src/Componentes/Menu.jsx
@@ -0,0 +1,14 @@
+import { link } from "react-router-dom"
+import "../styles/card.css"
+
+function Cards ({listaVideos}) {
+
+    return(
+
+       // listaVideos.map((video) =>
+        <div className = "card">
+           
+        </div>
+    )
+
+}
