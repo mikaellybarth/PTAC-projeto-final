3b9f677e4f497f0d2113c0b86d48218ea3c39a65
mikaellybarth <wwwmikasouza06@gmail.com>
2023-11-28 07:52:53 -0400

    commit sla

--- src/Componentes/Menu.jsx
+++ src/Componentes/Menu.jsx
@@ -5,10 +5,19 @@ function Cards ({listaVideos}) {
 
     return(
 
-       // listaVideos.map((video) =>
+       listaVideos.map((video) =>
         <div className = "card">
+           <iframe
+           width= "853"
+           height= "480"
+           onScroll={
+            'https://www.youtube.com/embed/' + video.url.slice(17)}
+            frameborder ="0"
+            allow = "accelerometer; autoplay; clipboard-write;">
+            </iframe>
            
         </div>
+        
     )
-
+    )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,5 @@
+import {link} from "react-router-dom";
+import Header from "./Componentes/Header";
 
 export default function Home(){
     return(
