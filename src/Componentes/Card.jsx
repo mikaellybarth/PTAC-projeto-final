3abf0478b225035859ae5f6960ec3ef4c374e002
mikaellybarth <wwwmikasouza06@gmail.com>
2023-12-01 14:22:01 -0400

    commit
    apareceu alguma coisa, mas tem erro ainda

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import '../style.css'
 
-function Card ({listaVideos}) {
+export default function Card ({listaVideos}) {
 
     return(
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,14 @@
 import {Link} from "react-router-dom";
 import React from "react";
 import Header from "./Componentes/Header";
-
+import Card from "./Componentes/Card";
 
 
 export default function Home(){
     return(
       <div>
       <Header/>
+      <Card listaVideos = {objeto}/>
       </div>
      );
 }
