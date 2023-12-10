2a39c59d1181f7cd8ab1901ea394f7dd577e0f8b
mikaellybarth <wwwmikasouza06@gmail.com>
2023-12-09 22:13:05 -0300

    commit
    so mudei o slice pra 4

--- src/Destaque.jsx
+++ src/Destaque.jsx
@@ -4,7 +4,7 @@ import Card from "./Componentes/Card";
 
 export default function Destaque(){
     const destaque = JSON.parse( localStorage.getItem ("ListaVideo")) || [];
-    const montrarOsDestaque = destaque.slice(-2)
+    const montrarOsDestaque = destaque.slice(-4)
     console.log(montrarOsDestaque)
     return(
       <div>
